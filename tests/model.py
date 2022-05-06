7345228206f61fa202e2352940969bd0e054d086
Remi <remi.delacourt@gmail.com>
2022-05-06 18:19:13 +0200

    refined code

---
+++ tests/model.py
@@ -0,0 +1,84 @@
+from tqdm.notebook import tqdm #Make sure to only import from tqdm and not from tqdm.notebook when leaving the notebook
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from unet import *
+
+class NoisyDataset(Dataset):
+    def __init__(self, X, Y):
+        assert X.shape == Y.shape and X.ndim == 4
+        self.X = X
+        self.Y = Y
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, index):
+        return self.X[index], self.Y[index]
+    
+class Model():
+    def __init__(self, model=None) -> None:
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        if model is None:
+            self.model = UNet(3, 3).to(self.device)
+        else:
+            self.model = model.to(self.device)
+        
+    def load_pretrained_model(self, filename="bestmodel") -> None:
+        checkpoint = torch.load(filename+".pth", map_location=self.device)
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+    
+    def save_trained_model(self, filename="bestmodel") -> None:
+        torch.save({'model_state_dict': self.model.state_dict()}, filename+".pth")
+        
+    def predict(self, test_input) -> torch.Tensor:
+        torch.cuda.empty_cache()
+        self.model.eval()
+        with torch.no_grad():
+            out = self.model(test_input.to(self.device))
+            return out
+    
+    def train_epoch(self, loader, optimizer, loss_fn) -> None:
+        scaler = None if not torch.cuda.is_available() else torch.cuda.amp.GradScaler() #Speedup computation tricks. See https://pytorch.org/docs/stable/amp.html
+        loop = tqdm(loader)
+
+        for data, targets in loop:
+            data = data.to(self.device)
+            targets = targets.to(self.device)
+
+            # forward
+            if scaler is not None:
+                with torch.cuda.amp.autocast(): #Speedup computation tricks. See https://pytorch.org/docs/stable/amp.html
+                    predictions = self.model(data)
+                    loss = loss_fn(predictions, targets)
+            else:
+                predictions = self.model(data)
+                loss = loss_fn(predictions, targets)
+
+            # backward
+            optimizer.zero_grad()
+            if scaler is not None:
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+            else:
+                loss.backward()
+                optimizer.step()
+
+            loop.set_postfix(loss=loss.item())
+
+    def train(self, train_input, train_target, optimizer=None, loss_fn=None, num_epochs=10, batch_size=100, learning_rate=10e-4) -> None:
+        torch.cuda.empty_cache()
+        dataset = NoisyDataset(train_input, train_target)
+
+        if loss_fn is None:
+            loss_fn = nn.MSELoss()
+        
+        if optimizer is None:
+            optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+
+        self.model.train()
+        for epoch in tqdm(range(num_epochs)):
+            dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+            self.train_epoch(dataloader, optimizer, loss_fn)
---
+++ tests/train.py
@@ -0,0 +1,41 @@
+import tqdm.notebook as tqdm
+import torch
+from model import *
+
+train_dir = "train_data.pkl"
+val_dir = "val_data.pkl"
+
+def compute_psnr(denoised, target, max_range=1.0):
+    assert denoised.shape == target.shape and denoised.ndim == 4
+    return 20 * torch.log10(torch.tensor(max_range)) - 10 * torch.log10(((denoised-target) ** 2).mean((1,2,3))).mean()
+
+def train_model(load_model=False, save_model=False, model=None, optimizer=None, loss_fn=None, batch_size=100, num_epochs=1):
+    model = Model(model=model)
+
+    train_input0, train_input1 = torch.load(train_dir)
+    val_input, val_target = torch.load(val_dir)
+
+    train_input0 = train_input0.float() / 255.0
+    train_input1 = train_input1.float() / 255.0
+    val_input = val_input.float() / 255.0
+    val_target = val_target.float() / 255.0
+
+    output_psnr_before = compute_psnr(val_input, val_target)
+
+    if load_model:
+        model.load_pretrained_model()
+    else:
+        model.train(train_input0, train_input1, optimizer=optimizer, loss_fn=loss_fn, num_epochs=num_epochs, batch_size=batch_size)
+        if save_model:
+            model.save_trained_model()
+
+    mini_batch_size = 100
+    model_outputs = []
+    for b in tqdm(range(0, val_input.size(0), mini_batch_size)):
+        output = model.predict(val_input.narrow(0, b, mini_batch_size))
+        model_outputs.append(output.cpu())
+    model_outputs = torch.cat(model_outputs, dim=0)
+
+    output_psnr_after = compute_psnr(model_outputs, val_target)
+    print(f"[PSNR : {output_psnr_after:.2f} dB], PSNR before training = {output_psnr_before:.2f} dB")
+    return model_outputs
